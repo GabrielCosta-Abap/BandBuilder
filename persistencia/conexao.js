7a84ae51ab1b20540f9c39a3ca0ac183a8bbe0a5
GabrielCosta-Abap <gabriel.costarosa.9@gmail.com>
2023-10-09 19:52:18 -0300

    deploy config conexão pg

--- persistencia/conexao.js
+++ persistencia/conexao.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: 'postgres',
   host: 'containers-us-west-134.railway.app',
   database: 'railway',
-  password: '***********',
+  password: 'Ep67p8HpnFsHgvRCLnVn',
   port: 7248,
 });
 
